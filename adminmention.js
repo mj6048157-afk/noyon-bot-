efcb7ab3503a17d52d1eb78f781ad381610aaeb0
mj6048157-afk <mj6048157@gmail.com>
2026-07-02 20:51:06 +0600

    Update adminmention.js

--- adminmention.js
+++ adminmention.js
@@ -13,7 +13,7 @@ module.exports = {
   onStart: async function () {},
 
   onChat: async function ({ event, message }) {
-    const adminIDs = ["100088489418721", "100088489418721", "100088489418721""].map(String);
+    const adminIDs = ["61559237171744", "61559237171744", "100088489418721""].map(String);
 
     // Skip if sender is admin
     if (adminIDs.includes(String(event.senderID))) return;
